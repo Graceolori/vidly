c520d9f61a943f3d10dc55eddf36dae5716fcc5f
graceolori <golori84@gmail.com>
2022-07-09 01:37:13 +0100

    input

--- src/components/navBar.jsx
+++ src/components/navBar.jsx
@@ -29,25 +29,15 @@ const NavBar = () => {
           <NavLink className="nav-item nav-link" to="rentals">
             Rentals
           </NavLink>
-          <NavLink className="nav-item nav-link disabled" to="/login">
+          <NavLink className="nav-item nav-link" to="/login">
             Login
+          </NavLink>
+          <NavLink className="nav-item nav-link" to="/register">
+            Register
           </NavLink>
         </div>
       </div>
     </nav>
-
-    //       <li className="nav-item">
-    //         <NavLink to="/customers">Customers</NavLink>
-    //       </li>
-    //       <li className="nav-item">
-    //         <NavLink to="/rentals">Rentals</NavLink>
-    //       </li>
-    //       <li className="nav-item">
-    //         <NavLink to="/login">Login</NavLink>
-    //       </li>
-    //     </div>
-    //   </div>
-    // </nav> */}
   );
 };
 
